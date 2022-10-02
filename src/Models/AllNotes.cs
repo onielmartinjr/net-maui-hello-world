db907f3d6a19caab2523a072b102e39ac7f9e40f
Oniel Martin <martin.oniel@icloud.com>
2022-10-01 20:38:46 -0400

    More Progress

---
+++ src/Models/AllNotes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiHelloWorldApp.Models
+{
+    internal class AllNotes
+    {
+        public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+
+        public AllNotes() => LoadNotes();
+
+        public void LoadNotes()
+        {
+            Notes.Clear();
+
+            // Get the folder where the notes are stored.
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            // Use Linq extensions to load the *.notes.txt files.
+            var notes = Directory
+
+                                        // Select the file names from the directory
+                                        .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                                        // Each file name is used to create a new Note
+                                        .Select(filename => new Note()
+                                        {
+                                            Filename = filename,
+                                            Text = File.ReadAllText(filename),
+                                            Date = File.GetCreationTime(filename)
+                                        })
+
+                                        // With the final collection of notes, order them by date
+                                        .OrderBy(note => note.Date);
+
+            // Add each note into the ObservableCollection
+            foreach (var note in notes)
+                Notes.Add(note);
+        }
+    }
+}
---
+++ src/Views/NotePage.xaml.cs
@@ -0,0 +1,56 @@
+using MauiHelloWorldApp.Models;
+
+namespace MauiHelloWorldApp.Views;
+
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
+public partial class NotePage : ContentPage
+{
+    public NotePage()
+	{
+		InitializeComponent();
+
+        var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+
+    public string ItemId
+    {
+        set { LoadNote(value); }
+    }
+
+    private void LoadNote(string fileName)
+    {
+        var noteModel = new Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
+    }
+
+    private async void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.Note note)
+            File.WriteAllText(note.Filename, TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
+
+        await Shell.Current.GoToAsync("..");
+    }
+}
